1e0d1807be6b6abdd42a73a3058ba6eec50cfb18
DaniGG832 <daniel.gonzalez.garcia@iesdonana.org>
2021-10-23 02:45:05 +0200

    ejercicios4

--- tema 3/3A04/js.js
+++ tema 3/3A04/js.js
@@ -1,5 +1,11 @@
-function miFunc() {
-    alert('Se ha dado clic al botón!');
+
+ document.write("<h2>Lista de 10 numeros aleatorios: </h2>");
+
+  for (let i = 0; i < 10; i++) {
+    
+    let azar = Math.floor((Math.random()* (20 - 0)) + 0);
+
+    document.write("<p>"+(i+1)+ " --> <span style='color: red;' >" + azar +"</span></p>");
   }
   
-  document.getElementById("12").onclick = miFunc;     
+ 
